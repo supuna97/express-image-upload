3b1b710f1ccd54ae3cf8e87fd031b7f875c6ef05
supuna97 <supunnanayakkara01@gmail.com>
2023-10-10 22:28:24 +0530

    Create database connection

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+});
+
+module.exports = pool;
