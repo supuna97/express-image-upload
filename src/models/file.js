c07476723a325e178745984454c430b31d951613
supuna97 <supunnanayakkara01@gmail.com>
2023-10-22 22:16:53 +0530

    Create file model

---
+++ src/models/file.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const db = require("../config/database");
+
+const File = db.define(
+  "File",
+  {
+    filename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "files",
+  }
+);
+
+module.exports = File;
