ddd42fe2e42c173079e569d0876517c3b3643abb
supuna97 <supunnanayakkara01@gmail.com>
2023-10-24 01:23:48 +0530

    Update file model

--- src/models/file.js
+++ src/models/file.js
@@ -6,7 +6,7 @@ const File = db.define(
   {
     filename: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     description: {
       type: DataTypes.STRING,
